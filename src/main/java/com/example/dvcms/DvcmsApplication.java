9f221a0fee8665c5cfb402125a40f691cceab208
Dipesh Sharma <dipesh.ss19@gmail.com>
2022-11-29 17:46:15 +0545

    Initial commit

---
+++ src/main/java/com/example/dvcms/DvcmsApplication.java
@@ -0,0 +1,13 @@
+package com.example.dvcms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DvcmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DvcmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dvcms/DvcmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dvcms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DvcmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
